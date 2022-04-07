781a9bba6f4ddcaeadf4b4401e1070ba2cd001a7
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2022-04-07 19:18:50 -0400

    Adding Routing v6

--- src/pages/RouterPage.js
+++ src/pages/RouterPage.js
@@ -5,39 +5,53 @@ import {
   VideoCameraOutlined,
   UploadOutlined,
 } from "@ant-design/icons";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import Login from "./Login";
+import Schedule from "./Schedule";
+import CreateTicket from "./CreateTicket";
+import Desktop from "./Desktop";
 
 const { Sider, Content } = Layout;
 
 const RouterPage = () => {
   return (
-    <Layout style={{ height: "100vh" }}>
-      <Sider>
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            Ingresar
-          </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            Cola
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            Crear Ticket
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Content
-          className="site-layout-background"
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-          }}
-        >
-          Content
-        </Content>
+    <BrowserRouter>
+      <Layout style={{ height: "100vh" }}>
+        <Sider hidden={false}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              <Link to="/login">Ingresar</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              <Link to="/schedule">Cola de tickets</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              <Link to="/create-ticket">Crear Ticket</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/schedule" element={<Schedule />} />
+              <Route path="/create-ticket" element={<CreateTicket />} />
+
+              <Route path="/desktop" element={<Desktop />} />
+              <Route path="*" element={<Navigate to="/login" replace />} />
+            </Routes>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </BrowserRouter>
   );
 };
 
